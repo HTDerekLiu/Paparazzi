dde5a67ac7c5db57379d5899e384f869751a06c9
Michael Tao <mtao@dgp.toronto.edu>
2018-12-04 16:03:30 +0000

    adding git repository support to our setup.py depenency pulling

--- setup.py
+++ setup.py
@@ -2,9 +2,19 @@
 
 from setuptools import setup
 
-def read(filename):
-    with open(filename,'r') as fd:
-        return fd.readlines()
+install_requires = []
+dependency_links = []
+with open("./requirements.txt",'r') as fd:
+    lines = map(lambda x: x.strip(),fd.readlines())
+    for line in lines:
+        if line[:2] == "-e":
+            link = line.split()[1]
+            egg=link.split("#egg=")[1]
+            print(link,"====",egg)
+            install_requires.append(egg)
+            dependency_links.append(link)
+        else:
+            install_requires.append(line)
 
 setup(name='paparazzi',
       version='0.1',
@@ -12,6 +22,7 @@ setup(name='paparazzi',
       author='Derek Liu, Michael Tao, Alec Jacobson',
       author_email='derek@dgp.toronto.edu, mtao@dgp.toronto.edu, jacobson@dgp.toronto.edu',
       url='http://www.dgp.toronto.edu/projects/paparazzi/',
-      install_requires=read("./requirements.txt"),
+      install_requires=install_requires,
+      dependency_links=dependency_links,
       packages=['paparazzi'],
       )
