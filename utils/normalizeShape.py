6e4262269a8c021cce4c51f0c4daffeb94901ceb
Michael Tao <mtao@dgp.toronto.edu>
2018-12-04 15:39:33 +0000

    updating readme and removing monolithic versions

--- utils/normalizeShape.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from vertexAreas import *
-
-def normalizeShape(V,F):
-    VA = vertexAreas(V,F)
-    Vmean = np.sum(V * VA[:,None],axis = 0) / np.sum(VA)
-    V = V-Vmean
-    V /= V.max()
-    return V
